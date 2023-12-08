4079024534889456e524c2a9bbff1c9cd806d4b3
pcan <3163405354@qq.com>
2023-12-08 23:16:29 +0800

    新增员工业务代码开发

---
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.sky.handler;
+
+import com.sky.constant.MessageConstant;
+import com.sky.exception.BaseException;
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.formula.constant.ErrorConstant;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * 全局异常处理器，处理项目中抛出的业务异常
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    /**
+     * 捕获业务异常
+     *
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(BaseException ex) {
+        log.error("异常信息：{}", ex.getMessage());
+        return Result.error(ex.getMessage());
+    }
+
+    /**
+     * 处理SQL异常
+     *
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+//      Duplicate entry 'zhangsan' for key 'idx_username'
+        String message = ex.getMessage();
+        //判断是否为用户名重复
+        if (message.contains("Duplicate entry")) {
+            String[] split = message.split(" ");
+            String msg = split[2];
+            msg = msg + MessageConstant.ALREADY_EXISTS;
+            return Result.error(msg);
+        }else {
+            //未知错误
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
+
+}
